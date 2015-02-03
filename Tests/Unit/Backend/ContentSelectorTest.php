f990a4d8a9f6f7cd84d566e509c857e2d63e33ca
Claus Due <claus@namelesscoder.net>
2015-02-03 01:15:14 +0100

    [TASK] Slightly improve test coverage

--- Tests/Unit/Backend/ContentSelectorTest.php
+++ Tests/Unit/Backend/ContentSelectorTest.php
@@ -8,6 +8,7 @@ namespace FluidTYPO3\Fluidcontent\Tests\Unit\Backend;
  * LICENSE.md file that was distributed with this source code.
  */
 
+use FluidTYPO3\Fluidcontent\Backend\ContentSelector;
 use TYPO3\CMS\Core\Tests\UnitTestCase;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
@@ -16,10 +17,37 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
  */
 class ContentSelectorTest extends UnitTestCase {
 
+	/**
+	 * @return void
+	 */
 	public function testCreatesInstance() {
 		$instance = GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\Object\\ObjectManager')
 			->get('FluidTYPO3\\Fluidcontent\\Backend\\ContentSelector');
 		$this->assertInstanceOf('FluidTYPO3\\Fluidcontent\\Backend\\ContentSelector', $instance);
+	}
+
+	/**
+	 * @return void
+	 */
+	public function testRenderFieldCreatesSelectTag() {
+		$GLOBALS['LANG'] = $this->getMock('TYPO3\\CMS\\Lang\\LanguageService', array('sL'));
+		$statement = $this->getMock('TYPO3\\CMS\\Core\\Database\\PreparedStatement', array('execute', 'free', 'fetch'), array(), '', FALSE);
+		$statement->expects($this->any())->method('execute')->willReturn(FALSE);
+		$statement->expects($this->any())->method('fetch')->willReturn(FALSE);
+		$statement->expects($this->any())->method('free');
+		$GLOBALS['TYPO3_DB'] = $this->getMock('TYPO3\\CMS\\Core\\Database\\DatabaseConnection',
+			array('exec_SELECTquery', 'prepare_SELECTquery'), array(), '', FALSE);
+		$GLOBALS['TYPO3_DB']->expects($this->any())->method('exec_SELECTquery')->willReturn(FALSE);
+		$GLOBALS['TYPO3_DB']->expects($this->any())->method('prepare_SELECTquery')->willReturn($statement);
+		$instance = new ContentSelector();
+		$parameters = array(
+			'itemFormElName' => 'foobar',
+			'itemFormElValue' => 'foovalue'
+		);
+		$parent = 'unused';
+		$rendered = $instance->renderField($parameters, $parent);
+		$this->assertStringStartsWith('<div><select', $rendered);
+		$this->assertContains($parameters['itemFormElName'], $rendered);
 	}
 
 }
