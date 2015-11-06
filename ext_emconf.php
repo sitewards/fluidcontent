a6044f5f4b00efe4e4e63ec0fdba0fc8bf36928a
Stefan Neufeind <typo3.neufeind@speedpartner.de>
2015-11-06 10:37:16 +0100

    [TASK] Add dependency to at least PHP 5.4.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,6 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '4.3.3',
 	'constraints' => array(
 		'depends' => array(
+			'php' => '5.4.0-5.6.99',
 			'typo3' => '6.2.0-7.5.99',
 			'flux' => '7.2.0-7.3.99',
 		),
