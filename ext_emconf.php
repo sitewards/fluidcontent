2b71a84e629877aa7fef5276f5e885599f8f1539
Claus Due <claus@namelesscoder.net>
2016-03-02 14:30:32 +0100

    [TASK] Set Flux, TYPO3 and PHP dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -18,7 +18,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_email' => 'claus@namelesscoder.net',
 	'author_company' => '',
 	'shy' => '',
-	'dependencies' => 'cms,flux',
+	'dependencies' => 'flux',
 	'conflicts' => '',
 	'priority' => '',
 	'module' => '',
@@ -32,9 +32,9 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '4.4.0',
 	'constraints' => array(
 		'depends' => array(
-			'php' => '5.5.0-5.6.99',
-			'typo3' => '7.6.0-7.6.99',
-			'flux' => '7.3.0-7.3.99',
+			'php' => '5.5.0-7.0.99',
+			'typo3' => '7.6.0-8.0.99',
+			'flux' => '7.3.0-7.99.99',
 		),
 		'conflicts' => array(
 		),
